e9df997605c67a7e1cf2343a7840ff25b14a9257
Leandro Souza <leandro2005souza@gmail.com>
2025-03-15 20:57:43 -0300

    carrossel de produtos OK

--- src/components/ProdutosRelacionados/ProdutosRelacionados.tsx
+++ src/components/ProdutosRelacionados/ProdutosRelacionados.tsx
@@ -1,11 +1,17 @@
-import {useRef} from "react";
+import { useRef } from "react";
 import "../../styles/produtos_relacionados.scss";
 import api from "./api/produtos.json";
+import arrowLeft from "../../images/icons/arrow-left.svg";
+import arrowRight from "../../images/icons/arrow-right.svg";
 
 function ProdutosRelacionados() {
     const carrosselRef = useRef<HTMLDivElement>(null);
     const produtos = api.products;
-    
+
+    const formatarPreco = (preco: number) => {
+        return preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    };
+
     const scroll = (direction: "left" | "right") => {
         if (carrosselRef.current) {
             const quantidadeScroll = 700;
@@ -19,18 +25,25 @@ function ProdutosRelacionados() {
 
     return (
         <div className="produtos-relacionados">
-            <button className="arrow" onClick={() => scroll("left")}>⬅</button>
+            <button className="arrow" onClick={() => scroll("left")}><img src={arrowLeft} alt="" /></button>
             <div className="produtos" ref={carrosselRef}>
                 {produtos.map((produto, index) => (
                     <div key={index} className="produto">
                         <img src={produto.photo} alt="" />
-                        <h3>{produto.productName}</h3>
-                        <p>{produto.descriptionShort}</p>
-                        <p className="preco">{produto.price}</p>
+                        <div className="infos">
+                            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+                            <div className="precos">
+                                <p className="preco-antigo">R$ 30,90</p>
+                                <p className="preco">R$ 28,90</p>
+                                <p className="parcelas">ou 2x de R$ 49,95 sem juros</p>
+                            </div>
+                            <p className="frete">Frete grátis</p>
+                        </div>
+                        <button className="compra-btn">COMPRAR</button>
                     </div>
                 ))}
             </div>
-            <button className="arrow" onClick={() => scroll("right")}>➡</button>
+            <button className="arrow" onClick={() => scroll("right")}><img src={arrowRight} alt="" /></button>
         </div>
     );
 }
